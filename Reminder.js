428612d7150cd30b1af4969886b7f74af1350a75
jaishreekori <51723304+jaishreekori@users.noreply.github.com>
2019-07-11 22:56:19 +0530

    Update Reminder.js

--- Reminder.js
+++ Reminder.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-    TouchableOpacity,
-    TimePickerAndroid,
-    DatePickerAndroid,
-    TextInput,
-    AsyncStorage
-} from 'react-native';
-// import AsyncStorage from '@react-native-community/async-storage';
+import { Text, View, StyleSheet, TouchableOpacity, TimePickerAndroid, DatePickerAndroid, TextInput, } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { StackActions, NavigationActions } from 'react-navigation';
 const reminderArr = [];
 
@@ -19,14 +10,14 @@ export default class Reminder extends Component {
         this.setDate = this.setDate.bind(this);
         this.state = {
             chosenDate: new Date(),
-            tasks: [
-                { chosenAndroidTime: '00:00' },
-                { text: '' },
-                {
-                    androidDate: `${new Date().getUTCDate()}/${new Date().getUTCMonth() +
-                        1}/${new Date().getUTCFullYear()}`
-                },
-            ]
+            tasks: [{
+                chosenAndroidTime: '00:00',
+                text: '',
+
+                androidDate: `${new Date().getUTCDate()}/${new Date().getUTCMonth() +
+                    1}/${new Date().getUTCFullYear()}`
+                ,
+            }]
         };
     }
     setDate(newDate) {
@@ -73,7 +64,6 @@ export default class Reminder extends Component {
         reminderArr.push(this.getReminder())
         this.storeData(reminderArr)
     }
-
     getReminder() {
         return (
             {
@@ -83,7 +73,6 @@ export default class Reminder extends Component {
             }
         )
     }
-
     storeData = async (value) => {
         console.log("storeData " + JSON.stringify(value))
         try {
@@ -93,45 +82,45 @@ export default class Reminder extends Component {
             console.log(e)
         }
     }
-
-    setValue = (value) => {
-        console.log("test " + JSON.stringify(value))
-        AsyncStorage.setItem('tasks', value);
-        console.log(this.state.tasks)
-    }
     saveData() {
         let task = this.getReminder();
-        alert(JSON.stringify(task));
+        // alert(JSON.stringify(task));
+        reminderArr.push(this.getReminder())
         this.props.navigation.navigate('ReminderList', {
             tasks: task
         });
+        this.storeData(task)
+        console.log('reminder' + JSON.stringify(task))
     }
     render() {
         return (
             <View style={styles.container}>
+                <Text style={styles.textView}>Reminder Date:</Text>
                 <TouchableOpacity
                     onPress={() => this.setDateAndroid()}
                     style={{ margin: 10, height: 50, width: "80%", borderWidth: 2, borderColor: 'black', marginBottom: 20, alignSelf: 'center' }}>
                     <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>{this.state.androidDate}</Text>
                 </TouchableOpacity>
+                <Text style={styles.textView}>Reminder Time:</Text>
                 <TouchableOpacity
                     onPress={() => this.setTimeAndroid()}
                     style={{ margin: 10, marginBottom: 20, height: 50, width: "80%", borderWidth: 2, borderColor: 'black', alignSelf: 'center' }}>
                     <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>{this.state.chosenAndroidTime}</Text>
                 </TouchableOpacity>
-                <TextInput style={{ height: 50, width: "80%", borderWidth: 2, borderColor: 'black', margin: 10, alignSelf: 'center' }}
+                <Text style={styles.textView}>Reminder Note:</Text>
+                <TextInput style={{ height: 50, width: "80%", borderWidth: 2, borderColor: 'black', margin: 10, alignSelf: 'center', fontSize: 20 }}
                     onChangeText={this.changeText}
                     value={this.state.text}
                 />
-                <TouchableOpacity style={{ height: 40, width: "40%", borderWidth: 2, backgroundColor: 'green', marginTop: 50, alignSelf: 'center' }}
+                {/* <TouchableOpacity style={{ height: 40, width: "40%", borderWidth: 2, backgroundColor: 'green', marginTop: 50, alignSelf: 'center' }}
                     onPress={this.saveReminder}>
                     <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>Save Reminder</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
                 <TouchableOpacity style={{ height: 40, width: "40%", borderWidth: 2, backgroundColor: 'green', marginTop: 50, alignSelf: 'center' }}
                     onPress={() => {
                         this.saveData()
                     }}>
-                    <Text style={{ fontSize: 19, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>Go To Reminder</Text>
+                    <Text style={{ fontSize: 19, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>Save Reminder</Text>
                 </TouchableOpacity>
             </View>
         );
@@ -141,7 +130,12 @@ export default class Reminder extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 50,
+        marginTop: 30,
     },
-
+    textView: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 30,
+        color: 'black'
+    }
 });
