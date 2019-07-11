9eae6e2b0d109fe2aba329581f29bbedd907167a
jaishreekori <51723304+jaishreekori@users.noreply.github.com>
2019-07-11 22:57:30 +0530

    Update route.js

--- route.js
+++ route.js
@@ -3,12 +3,14 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from './Home';
 import Reminder from './Reminder';
 import ReminderList from './ReminderList';
+import DetailScreen from './Details';
 
 
 const MainNavigator = createStackNavigator({
     Home: { screen: Home },
     Reminder: { screen: Reminder },
     ReminderList: { screen: ReminderList },
+    Details: { screen: DetailScreen },
     initialRouteName: 'Home',
 });
 
