95ffaa9faaa0583582f22b8ce0ff5443943a4222
jaishreekori <51723304+jaishreekori@users.noreply.github.com>
2019-07-12 13:17:25 +0530

    Update ReminderList.js

--- ReminderList.js
+++ ReminderList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { View, FlatList, Text, StyleSheet, TouchableOpacity, AsyncStorage } from 'react-native';
+// import AsyncStorage from '@react-native-community/async-storage';
 import { StackActions, NavigationActions } from 'react-navigation';
 
 export default class ReminderList extends Component {
@@ -11,32 +11,32 @@ export default class ReminderList extends Component {
         }
     }
     componentDidMount() {
-         this.getData()
-        let task = this.props.navigation.state.params.tasks;
-        let allTasks = this.state.tasks;
-        allTasks.push(task);
-        this.setState({ tasks: allTasks });
+        this.getData()
+        // let task = this.props.navigation.state.params.tasks;
+        // let allTasks = this.state.tasks;
+        // allTasks.push(task);
+        // this.setState({ tasks: allTasks });
     }
     getData = async () => {
         try {
             const value = await AsyncStorage.getItem('tasks')
-            console.log('test'+ value)
+            console.log(value)
             if (value !== null) {
-                this.setState({ tasks: [JSON.parse(value)] })
+                this.setState({ tasks: JSON.parse(value) })
             }
         } catch (e) {
             alert(e)
         }
     }
-    deleteTask = i => {
+    deleteTask = (i) => {
         this.setState(
             prevState => {
                 let tasks = prevState.tasks.slice();
                 tasks.splice(i, 1);
                 return { tasks: tasks };
             },
-            // () => Tasks.save(this.state.tasks)
         );
+        AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks))
     };
     viewReminder = (data) => {
         this.props.navigation.navigate('Details', { info: data })
@@ -45,59 +45,64 @@ export default class ReminderList extends Component {
         this.props.navigation.navigate('Reminder', { info: data })
     }
     render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.heading}>
-                    Upcoming Reminders
-                </Text>
-                <FlatList
-                    style={styles.list}
-                    data={this.state.tasks}
-                    renderItem={({ item, index }) =>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', width: "100%" }}>
-                            <View style={styles.listItemCont}>
-                                <Text style={styles.listItem}>
-                                    Task:  {item.text}
-                                </Text>
-                                <Text style={styles.listItem}>
-                                    Date: {item.androidDate}
-                                </Text>
-                                <Text style={styles.listItem}>
-                                    Time: {item.chosenAndroidTime}
-                                </Text>
-                            </View>
+        if (this.state.tasks.length) {
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.heading}>
+                        Upcoming Reminders
+                    </Text>
+                    <FlatList
+                        style={styles.list}
+                        data={this.state.tasks}
+                        renderItem={({ item, index }) =>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', width: "100%" }}>
+                                <View style={styles.listItemCont}>
+                                    <Text style={styles.listItem}>
+                                        Task:  {item.text}
+                                    </Text>
+                                    <Text style={styles.listItem}>
+                                        Date: {item.androidDate}
+                                    </Text>
+                                    <Text style={styles.listItem}>
+                                        Time: {item.chosenAndroidTime}
+                                    </Text>
+                                </View>
 
-                            <View style={{ flexDirection: 'column' }}>
-                                <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'purple', marginLeft: 30, marginBottom: 7 }}
-                                    onPress={() => this.viewReminder(item)}>
-                                    <Text style={styles.buttonView}>View</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'blue', marginLeft: 30, marginBottom: 7 }}
-                                    onPress={() => this.editReminder(item)}>
-                                    <Text style={styles.buttonView}>Edit</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'red', marginLeft: 30 }}
-                                    onPress={() => this.deleteTask(index)}>
-                                    <Text style={styles.buttonView}>Delete</Text>
-                                </TouchableOpacity>
-                            </View>
-                            {/* <TouchableOpacity
-                                    style={{ height: 20, width: 20, marginLeft: 5 }}
-                                    onPress={() => this.checkViewMethod(item, index)}>
-                                    {item.isSelected === true ?
-                                        <View style={{ height: 20, width: 20, backgroundColor: 'blue', marginLeft: 5 }}>
-                                        </View>
-                                        :
-                                        <View style={{ height: 20, width: 20, borderColor: 'black', borderWidth: 3, marginLeft: 5 }}></View>
-                                    }
-                                </TouchableOpacity> */}
-                            <View style={styles.hr} />
-                        </View>}
-                    extraData={this.state}
-                    keyExtractor={(item, index) => index.toString()}
-                />
-            </View>
-        );
+                                <View style={{ flexDirection: 'column' }}>
+                                    <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'purple', marginLeft: 30, marginBottom: 7 }}
+                                        onPress={() => this.viewReminder(item)}>
+                                        <Text style={styles.buttonView}>View</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'blue', marginLeft: 30, marginBottom: 7 }}
+                                        onPress={() => this.editReminder(item)}>
+                                        <Text style={styles.buttonView}>Edit</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'red', marginLeft: 30 }}
+                                        onPress={() => this.deleteTask(index)}>
+                                        <Text style={styles.buttonView}>Delete</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                {/* <TouchableOpacity
+                                        style={{ height: 20, width: 20, marginLeft: 5 }}
+                                        onPress={() => this.checkViewMethod(item, index)}>
+                                        {item.isSelected === true ?
+                                            <View style={{ height: 20, width: 20, backgroundColor: 'blue', marginLeft: 5 }}>
+                                            </View>
+                                            :
+                                            <View style={{ height: 20, width: 20, borderColor: 'black', borderWidth: 3, marginLeft: 5 }}></View>
+                                        }
+                                    </TouchableOpacity> */}
+                                <View style={styles.hr} />
+                            </View>}
+                        extraData={this.state}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                </View>
+            );
+        } else {
+            return (<View><Text>No data found</Text></View>);
+        }
+
     }
 }
 
